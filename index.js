14714a43bcfdc0d5a10c9e98327c9f99223d8d0e
nick paparo <nickpaparo1@gmail.com>
2025-01-27 15:13:26 -0500

    first heroku commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
+import mysql from "mysql2";
 import userRouter from "./routes/user-routes.js";
 import productRouter from "./routes/product-routes.js";
 import reservationRouter from "./routes/reservation-routes.js";
@@ -11,6 +12,22 @@ const PORT = process.env.PORT || 8080;
 
 app.use(cors({ origin: CROSS_ORIGIN }));
 app.use(express.json());
+
+app.get("/api/data", async (req, res) => {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  try {
+    const [rows] = await connection.execute("SELECT * FROM db_recoup");
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.use("/user", userRouter);
 app.use("/product", productRouter);
